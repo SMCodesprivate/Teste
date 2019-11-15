a44682542fc257a13eb8b1e22121d17bc94680f0
Glitch (hello-express) <none>
2019-11-15 19:58:41 +0000

    👻⛰ Checkpoint ./public/dashboard.js:6667416/1270 ./views/dashboard.html:6667416/1498

--- public/dashboard.js
+++ public/dashboard.js
@@ -6,11 +6,15 @@ var loads = document.querySelector("#load");
 function createLoading(daddy) {
 
   var load = document.createElement("div");
+  var loading = document.createElement("lottie-player")
+  loading.setAttribute("src", "https://assets2.lottiefiles.com/temp/lf20_tGD9tP.json");
+  loading.setAttribute("background", "transparent");
+  loading.setAttribute("speed", "0.7")
+  load.appendChild(loading);
   load.style.position = "fixed";
   load.style.width = "100%";
   load.style.height = "100%";
-  load.style.backgroundColor = "#444444";
-  load.style.float = "left";
+  load.style.backgroundColor = "#FFFFFF";
   loads.appendChild(load);
   setTimeout(() => {
     
@@ -19,9 +23,14 @@ function createLoading(daddy) {
   }, 10000)
 }
 
-corpo.onload = function() {
-  
-  createLoading(corpo);
-  
-}
+createLoading(corpo);
 
+
+// <lottie-player
+//  src="https://assets2.lottiefiles.com/temp/lf20_tGD9tP.json"
+//  background="transparent"
+//  speed="0.7"
+//  style="width: 300px; height: 300px;"
+//  loop
+//  autoplay >
+// </lottie-player>
