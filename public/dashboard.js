1b4543043f1bd2355bc39d11e641f350c4f59c79
Glitch (hello-express) <none>
2019-11-15 20:10:40 +0000

    🚠🍖 Checkpoint ./public/dashboard.js:6667416/3155 ./views/dashboard.html:6667416/1588

--- public/dashboard.js
+++ public/dashboard.js
@@ -2,22 +2,23 @@
 
 var corpo = document.querySelector("#corpoDashboard");
 var loads = document.querySelector("#load");
-
+var loadinging = document.querySelector("#loadinggg");
 function createLoading(daddy) {
 
   var load = document.createElement("div");
-  var loading = document.createElement("lottie-player")
-  loading.setAttribute("src", "https://assets2.lottiefiles.com/temp/lf20_tGD9tP.json");
-  loading.setAttribute("background", "transparent");
-  loading.setAttribute("speed", "0.7")
-  load.appendChild(loading);
   load.style.position = "fixed";
   load.style.width = "100%";
   load.style.height = "100%";
   load.style.backgroundColor = "#FFFFFF";
+  var teste = '<lottie-player id="loadinggg" src="https://assets2.lottiefiles.com/temp/lf20_tGD9tP.json" background="transparent" speed="0.7" style="width: 300px; height: 300px;margin-left: 50%;" loop autoplay></lottie-player>';
+  console.log(teste);
+  var htmlObject = document.createElement('div');
+  htmlObject.innerHTML = teste;
+  console.log(htmlObject);
+  load.appendChild(htmlObject);
   loads.appendChild(load);
-  setTimeout(() => {
     
+  setTimeout(() => {
     loads.removeChild(load);
     
   }, 10000)
