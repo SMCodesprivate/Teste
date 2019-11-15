f895e05dbadaf6881163f9ad29b2d71ac9a0a2cf
Glitch (hello-express) <none>
2019-11-15 20:22:43 +0000

    🌃😘 Checkpoint ./public/dashboard.js:6667416/1137

--- public/dashboard.js
+++ public/dashboard.js
@@ -10,28 +10,24 @@ function createLoading(daddy) {
   load.style.width = "100%";
   load.style.height = "100%";
   load.style.backgroundColor = "#FFFFFF";
-  var teste = '<lottie-player id="loadinggg" src="https://assets2.lottiefiles.com/temp/lf20_tGD9tP.json" background="transparent" speed="0.7" style="width: 300px; height: 300px;margin-left: 50%;" loop autoplay></lottie-player>';
+  var teste = '<lottie-player id="loadinggg" src="https://assets2.lottiefiles.com/temp/lf20_tGD9tP.json" background="transparent" speed="0.8" style="width: 300px;height: 300px;" loop autoplay></lottie-player>';
   console.log(teste);
   var htmlObject = document.createElement('div');
+  htmlObject.style.height = "90%";
+  htmlObject.style.display = "flex";
+  htmlObject.style.textAlign = "center";
+  htmlObject.style.justifyContent = "center";
+  htmlObject.style.alignItems = "center";
+// justify-content: center;
+// align-items: center
   htmlObject.innerHTML = teste;
   console.log(htmlObject);
   load.appendChild(htmlObject);
   loads.appendChild(load);
     
   setTimeout(() => {
-    loads.removeChild(load);
-    
-  }, 10000)
+    loads.removeChild(load);   
+  }, 5000)
 }
 
 createLoading(corpo);
-
-
-// <lottie-player
-//  src="https://assets2.lottiefiles.com/temp/lf20_tGD9tP.json"
-//  background="transparent"
-//  speed="0.7"
-//  style="width: 300px; height: 300px;"
-//  loop
-//  autoplay >
-// </lottie-player>
