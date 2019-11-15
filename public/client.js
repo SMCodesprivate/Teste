992c95f6c3838a87ba344511d05400e91fb2be8f
Glitch (hello-express) <none>
2019-11-15 18:58:41 +0000

    😘👟 Checkpoint ./public/client.js:6667416/2914 ./views/index.html:6667416/51 ./public/style.css:6667416/3374 ./server.js:6667416/2474

--- public/client.js
+++ public/client.js
@@ -1,6 +1,7 @@
 var emailInput = document.querySelector("#inputEmail");
 var formCadastro = document.querySelector("#formularioCadastro");
 var menuCadastro = document.querySelector("#menuCadastro");
+var corpoIndex = document.querySelector("#corpo");
 function getRandomColor() {
 	var letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWYZ";
 	var color = "";
@@ -14,19 +15,17 @@ function getRandomColorDark() {
 	color = colors[Math.floor(Math.random() * 3)];
 	return color;
 }
+corpoIndex.onload = function() {
+  menuCadastro.style.transition = "1.5s all linear";
+  emailInput.style.transition = "1.5s all ease"; 
+}
 var color = getRandomColor();
 emailInput.style.boxShadow = "0px 3px 0px #"+color;
 formCadastro.appendChild(emailInput);
-setInterval(trocarColor, 250)
+setInterval(trocarColor, 150)
 function trocarColor() {
 	var color = getRandomColorDark();
 	menuCadastro.style.border = "4px solid #"+color;
 	var colorId = getRandomColor();
-    emailInput.style.boxShadow =  "0px 3px 0px #"+colorId;
+  emailInput.style.boxShadow =  "0px 3px 0px #"+colorId;
 }
-// setInterval(() => {
-// 	var color = getRandomColorDark();
-// 	menuCadastro.style.border = "4px solid #"+color;
-//     var colorId = getRandomColor();
-//     emailInput.style.boxShadow =  "0px 3px 0px #"+colorId;
-// }, 950);
