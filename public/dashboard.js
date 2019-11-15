9a8bc0be42e3bb494246730a9ed1c46964f1ea5c
Glitch (hello-express) <none>
2019-11-15 19:46:40 +0000

    ✌️🍮 Checkpoint ./public/dashboard.js:6667416/1283 ./public/dashboardStyle.css:6667416/12 ./views/dashboard.html:6667416/239

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,14 +1,22 @@
 // alert("Teste");
 
 var corpo = document.querySelector("#corpoDashboard");
+var loads = document.querySelector("#load");
 
 function createLoading(daddy) {
 
   var load = document.createElement("div");
-  
+  load.style.position = "fixed";
+  load.style.width = "100%";
+  load.style.height = "100%";
   load.style.backgroundColor = "#444444";
-  daddy.appendChild(load);
-  
+  load.style.float = "left";
+  loads.appendChild(load);
+  setTimeout(() => {
+    
+    loads.removeChild(load);
+    
+  }, 10000)
 }
 
 corpo.onload = function() {
